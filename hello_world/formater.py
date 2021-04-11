e5db50e1bc66588856e63d8a939ff6d080c916ed
PatrycjaK <PatrycjaK@users.noreply.github.com>
2021-04-11 09:45:11 +0000

    XML

--- hello_world/formater.py
+++ hello_world/formater.py
@@ -21,3 +21,22 @@ def get_formatted(msg, imie, format):
     elif format == XML:
         result = format_to_xml(msg, imie)
     return result
+
+    def format_to_json(msg, imie):
+        return ('{ "imie":"' + imie + '", "msg":"' +
+                msg + '"}')
+
+    def format_to_xml(msg, imie):
+        return '<greetings><name>Patrycja</name> <msg>Hello World!</msg></greetings>'
+
+
+    def plain_text(msg, imie):
+        return imie + ' ' + msg
+
+
+    def plain_text_upper_case(msg, imie):
+        return plain_text(msg.upper(), imie.upper())
+
+
+    def plain_text_lower_case(msg, imie):
+        return plain_text(msg.lower(), imie.lower())
