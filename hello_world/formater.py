4f5e436774f23770ce7c59df6be868110a7572f7
PatrycjaK <PatrycjaK@users.noreply.github.com>
2021-04-11 09:52:20 +0000

    Format xml tests

--- hello_world/formater.py
+++ hello_world/formater.py
@@ -1,9 +1,15 @@
+import json
 
 PLAIN = "plain"
 PLAIN_UP = "plain_uppercase"
 PLAIN_LO = "plain_lowercase"
 JSON = "json"
 XML = "xml"
+
+x = {
+  "imie": "Patrycja",
+  "msg": "Hello World!"
+}
 
 SUPPORTED = [PLAIN, PLAIN_UP, PLAIN_LO, JSON, XML]
 
@@ -22,21 +28,24 @@ def get_formatted(msg, imie, format):
         result = format_to_xml(msg, imie)
     return result
 
-    def format_to_json(msg, imie):
-        return ('{ "imie":"' + imie + '", "msg":"' +
-                msg + '"}')
 
-    def format_to_xml(msg, imie):
-        return '<greetings><name>Patrycja</name> <msg>Hello World!</msg></greetings>'
+# def format_to_json(msg, imie):
+#     return ('{ "imie":"' + imie + '", "msg":"' + msg + '"}')
+
+def format_to_json(msg, imie):
+    y = json.dumps(x)
+    return y
+
+def format_to_xml(msg, imie):
+    return '<greetings><name>Patrycja</name> <msg>Hello World!</msg></greetings>'
+
+def plain_text(msg, imie):
+    return imie + ' ' + msg
 
 
-    def plain_text(msg, imie):
-        return imie + ' ' + msg
+def plain_text_upper_case(msg, imie):
+    return plain_text(msg.upper(), imie.upper())
 
 
-    def plain_text_upper_case(msg, imie):
-        return plain_text(msg.upper(), imie.upper())
-
-
-    def plain_text_lower_case(msg, imie):
-        return plain_text(msg.lower(), imie.lower())
+def plain_text_lower_case(msg, imie):
+    return plain_text(msg.lower(), imie.lower())
--- test/test_views.py
+++ test/test_views.py
@@ -15,7 +15,7 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
-        self.assertEqual(b'{ "imie":"Patrycja", "msg":"Hello World!"}', rv.data)
+        self.assertEqual(b'{"imie": "Patrycja", "msg": "Hello World!"}', rv.data)
 
     def test_msg_with_output2(self):
         rv = self.app.get('/?output=xml')
